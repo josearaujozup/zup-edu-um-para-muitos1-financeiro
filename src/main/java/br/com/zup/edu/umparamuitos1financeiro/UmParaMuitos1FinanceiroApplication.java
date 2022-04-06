68fb99a4831382eecf43500e60234c0d91c8b32f
Denes <jose.araujo@zup.com.br>
2022-04-06 09:43:17 -0300

    criando a aplicacao

---
+++ src/main/java/br/com/zup/edu/umparamuitos1financeiro/UmParaMuitos1FinanceiroApplication.java
@@ -0,0 +1,13 @@
+package br.com.zup.edu.umparamuitos1financeiro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UmParaMuitos1FinanceiroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UmParaMuitos1FinanceiroApplication.class, args);
+	}
+
+}
